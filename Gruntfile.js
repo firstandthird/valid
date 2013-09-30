f4b5901aa495f3ab3ec300faf2a53a28681f5902
Greg Allen <me@jga.me>
2013-09-29 21:03:31 -0700

    removed full dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask('script-dist', ['concat:dist', 'uglify:dist']);
   grunt.registerTask('script-full', ['concat:full', 'uglify:full']);
-  grunt.registerTask('scripts', ['jshint', 'bower', 'script-dist', 'script-full', 'clean:bower', 'mocha', 'bytesize']);
+  grunt.registerTask('scripts', ['jshint', 'bower', 'script-dist', 'clean:bower', 'mocha', 'bytesize']);
   grunt.registerTask('default', ['scripts']);
   grunt.registerTask('dev', ['connect:server', 'watch']);
 };
