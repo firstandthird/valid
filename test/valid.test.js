042dafa8f6c9bb8cea5f9f8db8d4fa5fb3730a38
Greg Allen <me@jga.me>
2013-09-29 21:05:33 -0700

    added tests. fixes #1

--- test/valid.test.js
+++ test/valid.test.js
@@ -1,4 +1,122 @@
 
 suite('valid', function() {
 
+  suite('required', function() {
+
+    var form;
+    setup(function() {
+      form = $('#form1');
+      form.find('input').val('');
+    });
+
+    test('input - empty', function() {
+      var errors = valid(form);
+
+      assert.equal(errors instanceof Array, true);
+      assert.equal(errors.length, 1);
+      assert.equal(typeof errors[0].element, "object");
+      assert.equal(errors[0].element.className, "f1i1");
+      assert.equal(errors[0].type, 'required');
+      assert.equal(errors[0].error, 'This is required');
+    });
+
+    test('input - filled', function() {
+      form.find('input').val('name');
+
+      var errors = valid(form);
+
+      assert.equal(errors, false);
+    });
+  });
+
+  suite('regex', function() {
+
+    var form;
+    setup(function() {
+      form = $('#form2');
+      form.find('input').val('');
+    });
+
+    test('input - empty', function() {
+      var errors = valid(form);
+
+      assert.equal(errors instanceof Array, true);
+      assert.equal(errors.length, 1);
+      assert.equal(typeof errors[0].element, "object");
+      assert.equal(errors[0].type, 'regex');
+
+    });
+
+    test('input - valid', function() {
+      form.find('input').val(10);
+
+      var errors = valid(form);
+
+      assert.equal(errors, false);
+
+    });
+
+    test('input - invalid', function() {
+      form.find('input').val('a');
+
+      var errors = valid(form);
+
+      assert.equal(errors instanceof Array, true);
+      assert.equal(errors.length, 1);
+      assert.equal(typeof errors[0].element, "object");
+      assert.equal(errors[0].type, 'regex');
+
+    });
+
+  });
+
+  suite('multiple fields', function() {
+
+    var form;
+    setup(function() {
+      form = $('#form3');
+      form.find('input').val('');
+    });
+
+    test('both empty', function() {
+      var errors = valid(form);
+
+      assert.equal(errors instanceof Array, true);
+      assert.equal(errors.length, 2);
+
+      assert.equal(typeof errors[0].element, "object");
+      assert.equal(errors[0].element.className, "f3i1");
+      assert.equal(errors[0].type, 'required');
+      assert.equal(errors[0].error, 'This is required');
+
+      assert.equal(typeof errors[1].element, "object");
+      assert.equal(errors[1].element.className, "f3i2");
+      assert.equal(errors[1].type, 'required');
+      assert.equal(errors[1].error, 'This is required');
+    });
+
+    test('one valid', function() {
+      form.find('input:eq(0)').val('name');
+      var errors = valid(form);
+
+
+      assert.equal(errors instanceof Array, true);
+      assert.equal(errors.length, 1);
+
+      assert.equal(typeof errors[0].element, "object");
+      assert.equal(errors[0].element.className, "f3i2");
+      assert.equal(errors[0].type, 'required');
+      assert.equal(errors[0].error, 'This is required');
+    });
+
+
+    test('both valid', function() {
+      form.find('input').val('name');
+      var errors = valid(form);
+
+      assert.equal(errors, false);
+    });
+
+  });
+
 });
