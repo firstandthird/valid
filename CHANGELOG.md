28f9ada2588833f15790740d851a8658e326e677
Greg Allen <me@jga.me>
2013-10-04 09:32:36 -0700

    Release 0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,40 @@
+
+0.1.0 / 2013-10-04 
+==================
+
+  * updated build scripts. fixes #14 #13 #7
+  * Remove @todo
+  * Add comments within code
+  * Change method to getMatch()
+  * Add example to support new url attribute
+  * Add support for url test
+  * Add example for valid-email
+  * Add support for data-valid-email
+  * Remove whitespace from top of file
+  * Update example file to match code updates
+  * Update readme file
+  * changed to data-valid-required and data-valid-pattern.  fixes #6
+  * little refactor
+  * got all tests passing.  fixes #3
+  * updated tests to use pattern instead of regex
+  * fixed pattern check
+  * updated style to be consistent
+  * updated to use 2 spaces
+  * Merge branch 'develop'
+  * added a few pending tests
+  * changed element to pass back native element rather than jquery'd
+  * added tests. fixes #1
+  * reset version back to 0.0.0 on package (version not used)
+  * ignore jquery from concat-bower
+  * removed changelog (for now)
+  * added jquery to bower deps
+  * removed full dist
+  * Merge branch 'release/20130929.1' into develop
+  * Merge branch 'release/20130929.1'
+  * Change history.md to changelog.md
+  * Bump version; add dist library
+  * Merge branch 'feature/initial-work' into develop
+  * Add example file to illustrate library code
+  * Add library code
+  * usage in readme
+  * project setup
--- dist/valid.js
+++ dist/valid.js
@@ -5,52 +5,84 @@
  * copyright First + Third 2013
  * MIT License
 */
-//valid.js
+var valid = function(el) {
+  // jquery the selector
+  var $el = $(el);
+  // narrow selection to only these validations
+  var $els = $el.find('[data-valid-required], [data-valid-pattern], [data-valid-email]');
+  // storage array to return
+  var arr = [];
 
-var valid = function( el ) {
+  // tests the pattern against the value that is passed
+  // in, returns true if there is a match
+  var getMatch = function( value, pattern ) {
+      var regex = new RegExp(pattern);
+      return regex.test(value);
+  };
+
+  // check for errors with the passed in element
+  var getError = function(element) {
     // jquery the selector
-    var $el = $( el );
-    // narrow selection to only these validations
-    var $els = $el.find( '[data-pattern], [data-required]' );
-    // storage array to return
-    var arr = [];
+    var $element = $(element);
+    // trim the whitespace on inputs
+    var $value = $.trim($element.val());
+    // set the default error
+    var error = false;
+    // the type; pattern, email, url, or required
+    var type; 
+    // the regex pattern to test
+    var pattern;
 
-    // check for errors
-    var hasError = function( element ) {
-        // jquery the selector
-        var $element = $( element );
-        // trim the whitespace on inputs
-        var $value = $.trim( $element.val() );
-        // set the default error
-        var error = false;
+    // test the regex pattern
+    if ($element.data('valid-pattern')) {
+      type = 'pattern';
+      // grab the pattern that is passed in from the view
+      pattern = $element.data('valid-pattern');
+      // test() will return true for a match
+      // want to reverse that so it reads like english
+      // i.e. if there is an error, set error to true
+      error = ! getMatch($element.val(), pattern);
+    }
+    // test the url pattern
+    if ($element.data('valid-url')) {
+        type = 'url';
+        pattern = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+        error = ! getMatch($element.val(), pattern);
+    }
+    // test the email pattern
+    if ($element.data('valid-email') === '') {
+        type = 'email';
+        // this pattern came from a site that specified that
+        // this will match the RFC 5322 spec
+        pattern = /[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+        //run the pattern check with this pattern
+        // needs to return true if there is an error
+        error = ! getMatch($element.val(), pattern);
+    }
 
-        // test the regex pattern
-        if ( $element.data( 'pattern' ) ) {
-            var regex = new RegExp( $element.data( 'pattern' ) );
-            // test() will return true for a match
-            // want to reverse that so it reads like english
-            // i.e. if there is an error, set error to true
-            error = ! regex.test( element.value );
-        }
+    // this will check for an empty value
+    // covers the required case
+    if ($element.data('valid-required') === '' && $value.length === 0) {
+      type = 'required';
+      error = true;
+    }
 
-        // this will check for an empty value
-        // covers the required case
-        if ( $value.length === 0 ) {
-            error = true;
-        }
+    if (!error) {
+      return false;
+    }
 
-        return error;
+    return {
+      'element' : $element[0],
+      'type' : type
     };
+  };
 
-    $.each( $els, function() {
-        var $el = $( this );
-        if ( hasError( $el ) ) {
-            arr.push({
-                'type' : $el.attr( 'type' ),
-                'element' : $el,
-                'error' : ( $el.data( 'required' ) !== null ) ? 'required' : 'invalid'
-            });
-        }
-    });
-    return arr;
+  $.each($els, function() {
+    var $el = $(this);
+    var err = getError($el);
+    if (err) {
+      arr.push(err);
+    }
+  });
+  return arr;
 };
