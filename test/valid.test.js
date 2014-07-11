5cfd1d7dd2899c130b4af4261366fe546e08ba47
Troy Whiteley <troy@somany.us>
2014-07-10 19:00:44 -0700

    Fidelified.
    Issue #19

--- test/valid.test.js
+++ test/valid.test.js
@@ -3,280 +3,116 @@ suite('valid', function() {
 
   suite('init', function() {
     test('function exists', function() {
-      assert.ok(typeof valid === 'function');
-    });
-
-    test('check function exists', function() {
-      var test = new valid($('form'));
-      assert.ok(typeof test.check === 'function');
-    });
-
-    test('validate function exists', function() {
-      assert.ok(typeof valid.custom === 'function');
-    });
-
-    test('required function exists', function() {
-      assert.ok(typeof valid.required === 'function');
-    });
-
-    test('email function exists', function() {
-      assert.ok(typeof valid.email === 'function');
-    });
-
-    test('password function exists', function() {
-      assert.ok(typeof valid.password === 'function');
-    });
-
-    test('passwordConfirm function exists', function() {
-      assert.ok(typeof valid.passwordConfirm === 'function');
+      assert.ok(typeof $.fn.valid === 'function');
     });
   });
 
   suite('custom validator', function() {
     test('custom validator is function', function() {
-      var test = new valid($('form'), {
-        phone: function(input) {
-          return valid.custom(input, function(text) {
-            return text.length > 0;
-          }, 'phone');
+      $('form').valid({
+        customValidators: {
+          phone: function(input) {
+            return input;
+          }
         }
       });
 
-      console.log(validations);
-
-      assert.ok(typeof validations.phone === 'function');
-    });
-  });
-
-  suite('validations', function() {
-    suite('validate', function() {
-      test('should validate input with regex', function() {
-        var result = valid.custom($('#validate input'), /Test/);
-        assert.ok(result.valid);
-      });
-      test('should fail input with regex', function() {
-        var result = valid.custom($('#validate input'), /Fail/);
-        assert.ok(!result.valid);
-      });
-      test('should validate input with function', function() {
-        var result = valid.custom($('#validate input'), function(input) {
-          return input === 'Test';
-        });
-        assert.ok(result.valid);
-      });
-      test('should fail input with function', function() {
-        var result = valid.custom($('#validate input'), function(input) {
-          return input === 'Fail';
-        });
-        assert.ok(!result.valid);
-      });
-      test('should validate input with string', function() {
-        var result = valid.custom($('#validate input'), 'Test');
-        assert.ok(result.valid);
-      });
-      test('should fail input with string', function() {
-        var result = valid.custom($('#validate input'), 'Fail');
-        assert.ok(!result.valid);
-      });
-      test('should validate string with string', function() {
-        var result = valid.custom('Test', 'Test');
-        assert.ok(result.valid);
-      });
+      assert.equal($('form').valid('phone', 'test'), 'test');
     });
 
-    suite('required', function() {
-      test('should pass with text', function() {
-        var result = valid.required($('#required .filled'));
-        assert.ok(result.valid);
-      });
-      test('should fail with empty text', function() {
-        var result = valid.required($('#required .empty'));
-        assert.ok(!result.valid);
-      });
-    });
-
-    suite('email', function() {
-      test('should pass valid email', function() {
-        var result = valid.email('someone@somewhere.com');
-        assert.ok(result.valid);
-      });
-      test('should fail with invalid email', function() {
-        var result = valid.email('invalid email');
-        assert.ok(!result.valid);
-      });
-    });
-
-    suite('password', function() {
-      test('password should pass default method', function() {
-        var result = valid.password('wqR2d4cnCveB');
-        assert.ok(result.valid);
-      });
-      test('password should fail default method', function() {
-        var result = valid.password('pass');
-        assert.ok(!result.valid);
-      });
-      test('password should pass custom method', function() {
-        var result = valid.password('wqR2d4cnCveB', function(text) {
-          return text !== 'test';
-        });
-        assert.ok(result.valid);
-      });
-    });
-
-    suite('passwordConfirm', function() {
-      test('matching passwords should pass', function() {
-        var result = valid.passwordConfirm('wqR2d4cnCveB', 'wqR2d4cnCveB');
-        assert.ok(result.valid);
-      });
-      test('non-matching passwords should fail', function() {
-        var result = valid.passwordConfirm('wqR2d4cnCveB', 'doesntwork');
-        assert.ok(!result.valid);
-      });
-      test('input with data-valid-password-confirm should match passed input', function() {
-        var input = $('#password-confirm');
-
-        assert.ok(valid.passwordConfirm(input).valid);
-      });
-    });
-  });
-
-  suite('form runner', function() {
-    test('should return true on valid form', function() {
-      var result = new valid($('#valid'));
-      assert.ok(result.check());
-    });
-
-    test('should return array of invalid inputs on invalid form', function() {
-      var result = new valid($('#invalid'), {
-        bio: function(input) {
-          return valid.custom(input, function(text) {
-            return text.length > 0;
-          }, 'bio');
+    test('custom validator should have access to methods', function() {
+      $('form').valid({
+        customValidators: {
+          phone: function(input) {
+            return typeof this.validate === 'object';
+          }
         }
       });
 
-      assert.equal(result.check().length, 8);
-    });
-
-    test('should return element of failed item', function() {
-      var result = new valid($('#invalid'), {
-        bio: function(input) {
-          return valid.custom(input, function(text) {
-            return text.length > 0;
-          }, 'bio');
-        }
-      });
-
-      assert.ok(result.check()[0].element);
-    });
-
-    test('should return validation type of failed item', function() {
-      var result = new valid($('#invalid'), {
-        bio: function(input) {
-          return valid.custom(input, function(text) {
-            return text.length > 0;
-          }, 'bio');
-        }
-      });
-
-      assert.ok(result.check()[0].type);
+      assert.ok($('form').valid('phone'));
     });
   });
 
   suite('events', function() {
-    test('invalid', function(done) {
+    test('inputInvalid', function(done) {
       var form = $('#invalid');
-      var result = new valid(form);
+      form.valid();
 
-      form.on('invalid', function(input, data) {
+      form.on('inputInvalid', function(input, data) {
         assert.ok(input);
         assert.ok(data);
-        form.unbind('invalid');
-        done();
-      });
-
-      result.check();
-    });
-
-    test('valid', function(done) {
-      var form = $('#valid');
-      var result = new valid(form);
-
-      form.on('valid', function() {
-        assert.ok(true);
-        form.unbind('valid');
-        done();
-      });
-
-      result.check();
-    });
-
-    test('pass', function(done) {
-      var form = $('#valid');
-      var result = new valid(form);
-
-      form.on('pass', function(input) {
-        assert.ok(input);
-        form.unbind('pass');
-        done();
-      });
-
-      form.find('input').first().trigger('blur');
-    });
-
-    test('fail', function(done) {
-      var form = $('#invalid');
-      var result = new valid(form);
-
-      form.on('fail', function(input, data) {
-        assert.ok(input);
-        assert.ok(data);
-        form.unbind('fail');
-        done();
-      });
-
-      form.find('input').first().trigger('blur');
-    });
-
-    test('passing', function(done) {
-      var form = $('#valid');
-      var result = new valid(form);
-
-      form.on('passing', function(input) {
-        assert.ok(input);
-        form.unbind('passing');
-        done();
-      });
-
-      form.find('input').first().trigger('input');
-    });
-
-    test('failing', function(done, data) {
-      var form = $('#invalid');
-      var result = new valid(form);
-
-      form.on('failing', function(input, data) {
-        assert.ok(input);
-        assert.ok(data);
-        form.unbind('failing');
-        done();
-      });
-
-      form.find('input').first().trigger('input');
-    });
-
-    test('submit', function(done, data) {
-      var form = $('#invalid');
-      var result = new valid(form);
-
-      form.on('invalid', function(input, data) {
-        assert.ok(input);
-        assert.ok(data);
-        form.unbind('invalid');
+        form.unbind('inputInvalid');
         done();
       });
 
       form.trigger('submit');
+    });
+
+    test('inputValid', function(done) {
+      var form = $('#valid');
+      form.valid();
+
+      form.on('inputValid', function() {
+        assert.ok(true);
+        form.unbind('inputValid');
+        done();
+      });
+
+      form.trigger('submit');
+    });
+
+    test('inputPass', function(done) {
+      var form = $('#valid');
+      form.valid();
+
+      form.on('inputPass', function(input) {
+        assert.ok(input);
+        form.unbind('inputPass');
+        done();
+      });
+
+      form.find('input').first().trigger('blur');
+    });
+
+    test('inputFail', function(done) {
+      var form = $('#invalid');
+      form.valid();
+
+      form.on('inputFail', function(input, data) {
+        assert.ok(input);
+        assert.ok(data);
+        form.unbind('inputFail');
+        done();
+      });
+
+      form.find('input').first().trigger('blur');
+    });
+
+    test('inputPassing', function(done) {
+      var form = $('#valid');
+      form.valid();
+
+      form.on('inputPassing', function(input) {
+        assert.ok(input);
+        form.unbind('inputPassing');
+        done();
+      });
+
+      form.find('input').first().trigger('input');
+    });
+
+    test('inputFailing', function(done, data) {
+      var form = $('#invalid');
+      form.valid();
+
+      form.on('inputFailing', function(input, data) {
+        assert.ok(input);
+        assert.ok(data);
+        form.unbind('inputFailing');
+        done();
+      });
+
+      form.find('input').first().trigger('input');
     });
   });
 
